df7c30e92430aa07982378288fb7c7d750db0514
varun.singh <varunpratap74358@gmail.com>
2026-07-18 21:28:43 +0530

    port change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,17 +4,17 @@ import './App.css'
 import axios from 'axios'
 
 function App() {
-  const BACKEND_API = 'http://localhost:8080'
-  const [data,setData] = useState<any>({})
+  const BACKEND_API = 'http://localhost:8081'
+  const [data, setData] = useState<any>({})
 
-  const apidata = async()=>{
+  const apidata = async () => {
     const api = await axios.get(`${BACKEND_API}/api/verison`)
     setData(api.data)
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     apidata()
-  },[])
+  }, [])
 
   return (
     <>
